35ca1908cc810d23d0d9ec7e3713962598527eac
Kai0KenX20 <49903983+Kai0KenX20@users.noreply.github.com>
2025-08-11 12:52:22 -0400

    Add stubs for new AltStore app endpoints

--- appdb/API/API.swift
+++ appdb/API/API.swift
@@ -11,7 +11,7 @@ import SwiftyJSON
 import Localize_Swift
 
 enum API {
-    static let endpoint = "https://api.dbservices.to/v1.6/"
+    static let endpoint = "https://api.dbservices.to/v1.7/"
     static let statusEndpoint = "https://status.dbservices.to/API/v1.0/"
     static let itmsHelperEndpoint = "https://dbservices.to/manifest.php"
 
@@ -140,6 +140,9 @@ enum Actions: String {
     case getAltStoreRepos = "get_altstore_repos"
     case editAltStoreRepo = "edit_altstore_repo"
     case deleteAltStoreRepo = "delete_altstore_repo"
+    case getAltStoreOfficialApps = "get_altstore_official_apps"
+    case getAltStoreRepoApps = "get_altstore_repo_apps"
+    case getAltStoreUserApps = "get_altstore_user_apps"
     case getPlusPurchaseOptions = "get_plus_purchase_options"
     case getSideloadingOptions = "get_sideloading_options"
     case getFeatures = "get_features"
---
+++ appdb/Models/AltStoreNewApps.swift
@@ -0,0 +1,27 @@
+//
+//  AltStoreNewApps.swift
+//  appdb
+//
+//  Created by ChatGPT on 2024-07-??.
+//
+
+import ObjectMapper
+
+class OfficialAltStoreApp: AltStoreApp {
+    required init?(map: Map) {
+        super.init(map: map)
+    }
+}
+
+class RepoAltStoreApp: AltStoreApp {
+    required init?(map: Map) {
+        super.init(map: map)
+    }
+}
+
+class UserAltStoreApp: AltStoreApp {
+    required init?(map: Map) {
+        super.init(map: map)
+    }
+}
+
